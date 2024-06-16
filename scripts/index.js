b4f3725f28a724935ef8ffb5001cfea7188e7255
ProphesierC <bwknowles98@gmail.com>
2024-06-16 15:56:23 -0400

    update index.html   - devloped html skeleton initiated index.js initiated index.css

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+/*
+  IPO Chart
+  
+  Author: ProphesierC
+  Date: 6/16/2024
+  Modified: []
+
+  Input:
+    - [Define input variables here]
+
+  Process:
+    - [Define processing logic here]
+
+  Output:
+    - [Define output variables here]
+*/
