228afa7d4a8b19801b1e2dd71135767995bb29ea
ProphesierC <bwknowles98@gmail.com>
2024-06-18 00:40:20 -0400

    update index.html, scripts/index.js and styles/index.css completed main work

--- scripts/index.js
+++ scripts/index.js
@@ -3,14 +3,46 @@
   
   Author: ProphesierC
   Date: 6/16/2024
-  Modified: []
+  Modified: 6/17/2024
 
-  Input:
-    - [Define input variables here]
-
-  Process:
-    - [Define processing logic here]
-
-  Output:
-    - [Define output variables here]
 */
+
+class Elements 
+{
+  constructor() 
+  {
+    this.fetchJSON();
+  }
+
+  fetchJSON() //gets json data
+  {
+    fetch('./data.json') // fetchs url
+      .then(response => response.json()) // parses json
+      .then(data => { 
+        this.updateResults(data);
+      })
+  }
+
+  updateResults(results) 
+  {
+    results.forEach(result => 
+    {
+      const category = result.category.toLowerCase();
+      const icon = document.getElementById(`${category}-icon`);
+      const ratioP = document.getElementById(`${category}-ratio`);
+
+      if (icon) 
+      {
+        icon.src = result.icon;
+        icon.alt = `${result.category} Icon`;
+      }
+
+      if (ratioP) 
+      {
+        ratioP.textContent = `${result.score} `;
+      }
+    });
+  }
+}
+
+const element = new Elements();
